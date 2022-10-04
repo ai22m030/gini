66ec82bda7de444e6f2ea17e795fee1a63ba84b6
ai22m030 <ai22m030@technikum-wien.at>
2022-10-04 16:05:43 +0200

    refactor

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import numpy as np
 import pandas as pd
 import itertools
-import warnings
-warnings.filterwarnings(action='ignore')
 
 
-class Node:
+class Gini:
     def __init__(self, feat, tar):
         self.features = feat
         self.target = tar
-        self.feature_types = self.select_types()
-
-    # Check feature types
-    def select_types(self):
-        feature_types = []
-        for item in self.features.columns:
-            if self.features[item].dtype != "O":
-                feature_types.append((item, "numerical"))
-            else:
-                if self.features[item].nunique() <= 2:
-                    feature_types.append((item, "binary"))
-                else:
-                    feature_types.append((item, "multiclass"))
-        return feature_types
 
     # Calculate gini impurity for each feature at a node
     @staticmethod
@@ -39,7 +23,7 @@ class Node:
 
     # Calculate gini for all feature combinations in categorical features
     def calculate_gini(self, feature):
-        gini_node = []
+        gini_values = []
         combinations = []
 
         for i in range(1, self.features[feature].nunique()):
@@ -49,12 +33,12 @@ class Node:
             t1 = self.target[self.features[feature].isin(item)]
             t2 = self.target[~self.features[feature].isin(item)]
             args = t1.value_counts().tolist() + t2.value_counts().tolist()
-            gini_node.append(Node.gini_impurity_total(*args))
+            gini_values.append(Gini.gini_impurity_total(*args))
 
-        return gini_node, combinations  # Return all the values
+        return gini_values, combinations  # Return all the values
 
     # Get the best gini values for each feature
-    def check_nodes(self):
+    def check_features(self):
         gini_values = []
         for feature in self.features.columns:
             calculated_gini, combinations = self.calculate_gini(feature)
@@ -63,8 +47,8 @@ class Node:
         return gini_values
 
     # Get best node
-    def best_node(self):
-        gini_values = self.check_nodes()
+    def best_feature(self):
+        gini_values = self.check_features()
         values = [item[2] for item in gini_values]
 
         best_feature = gini_values[np.argmin(values)][0]
@@ -78,8 +62,7 @@ features = titanic.loc[:, ["Pclass", "Sex", "Embarked"]]
 features.Embarked.fillna(features.Embarked.mode()[0], inplace=True)
 features.Pclass = features.Pclass.map({1: "1st", 2: "2nd", 3: "3rd"})
 
-# Reading and cleaning the data
-root = Node(features, target)
+# Reading data
+root = Gini(features, target)
 
-# Building the tree
-root.best_node()
+root.best_feature()
