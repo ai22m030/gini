75f27975dd04a41ebb23702855842bdee66f7cd3
ai22m030 <ai22m030@technikum-wien.at>
2022-10-04 14:54:48 +0200

    best node calc

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Importing the required packages
 import numpy as np
 import pandas as pd
 import itertools
@@ -6,17 +5,14 @@ import warnings
 warnings.filterwarnings(action='ignore')
 
 
-# Main class
 class Node:
     def __init__(self, feat, tar):
-        self.left = None
-        self.right = None
         self.features = feat
         self.target = tar
-        self.feature_types = self.select_dtype()
+        self.feature_types = self.select_types()
 
-    # Identiy feature types
-    def select_dtype(self):
+    # Check feature types
+    def select_types(self):
         feature_types = []
         for item in self.features.columns:
             if self.features[item].dtype != "O":
@@ -29,7 +25,6 @@ class Node:
         return feature_types
 
     # Calculate gini impurity for each feature at a node
-    # Need to provide a default value as a fallback
     @staticmethod
     def gini_impurity_total(a=0, b=0, c=0, d=0):
         total_elements = a + b + c + d
@@ -59,7 +54,7 @@ class Node:
         return gini_node, combinations  # Return all the values
 
     # Get the best gini values for each feature
-    def evaluate_node(self):
+    def check_nodes(self):
         gini_values = []
         for feature in self.features.columns:
             calculated_gini, combinations = self.calculate_gini(feature)
@@ -67,42 +62,16 @@ class Node:
             gini_values.append((feature, best_combination, np.min(calculated_gini)))
         return gini_values
 
-    # Inserting a new node based on the decision criteria
-    def insert_node(self):
-        gini_values = self.evaluate_node()
+    # Get best node
+    def best_node(self):
+        gini_values = self.check_nodes()
         values = [item[2] for item in gini_values]
         node_gini = Node.gini_impurity(*self.target.value_counts().tolist())
 
-        # Terminate the branch if current gini is better or no features to split
-        if node_gini < np.min(values):
-            print("terminating the branch")
-            self.left = None
-            self.right = None
-        else:
+        if node_gini > np.min(values):
             best_feature = gini_values[np.argmin(values)][0]
             best_combination = gini_values[np.argmin(values)][1]
-            print(f"Creating a new branch using {best_feature} and {best_combination}")
-
-            left_features = self.features[self.features[best_feature].isin(best_combination)]
-            left_features.drop([best_feature], axis=1, inplace=True)
-            left_target = self.target[self.features[best_feature].isin(best_combination)]
-
-            if not list(left_features.columns):
-                self.left = None
-                self.right = None
-            else:
-                self.left = Node(left_features, left_target)
-                self.left.insert_node()
-
-            right_features = self.features[~self.features[best_feature].isin(best_combination)]
-            right_target = self.target[~self.features[best_feature].isin(best_combination)]
-            right_features.drop([best_feature], axis=1, inplace=True)
-            if not list(right_features.columns):
-                self.left = None
-                self.right = None
-            else:
-                self.right = Node(right_features, right_target)
-                self.right.insert_node()
+            print(f"Best node {best_feature} and yes/no decision {best_combination[0]}")
 
 
 titanic = pd.read_csv("train.csv")
@@ -114,7 +83,5 @@ features.Pclass = features.Pclass.map({1: "1st", 2: "2nd", 3: "3rd"})
 # Reading and cleaning the data
 root = Node(features, target)
 
-print(root.evaluate_node())
-
 # Building the tree
-root.insert_node()
+root.best_node()
